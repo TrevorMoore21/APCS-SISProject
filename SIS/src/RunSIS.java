502fb54fe344a7a0da7ae032885f9b4c00ed31d1
Andrew-M3373 <55599662+Andrew-M3373@users.noreply.github.com>
2021-10-26 11:41:21 -0600

    Update RunSIS.java

--- SIS/src/RunSIS.java
+++ SIS/src/RunSIS.java
@@ -5,7 +5,7 @@ public class RunSIS {
 		
 		System.out.println("Hello World");
 		System.out.println("I'm always here.");
-    System.out.println("Trev says hi");
+   		System.out.println("Trev says hi");
 		System.out.println("This is Bens Branch");
 		System.out.println("Kaitlyn was here");
 
