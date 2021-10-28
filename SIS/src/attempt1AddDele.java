87f5f3334455728c98d952d1c3b834a7ba877bf2
trevor.moore <trevor.moore@MU-C1-03.mullen.pvt.k12.co.us>
2021-10-28 11:42:45 -0600

    almost done with deleting students, I isolated names and paired that with the text file

--- SIS/src/attempt1AddDele.java
+++ SIS/src/attempt1AddDele.java
@@ -5,22 +5,60 @@ import java.io.IOException;
 
 public class attempt1AddDele
 	{
-		
 		static Scanner userIntInput = new Scanner(System.in);
-		static Scanner myCredit = new Scanner(new File("??????"));
+		static Scanner userStringInput = new Scanner(System.in);
 		
 		
+		public static void main(String[] args) throws IOException
+			{
+				//practice running da code
+			}
+		
+
 		public static void choice1() throws IOException
 		{
 			//whatever user chooses on main menu
 			int step1 = userIntInput.nextInt(); 
-		
+			Scanner myClass = new Scanner(new File("Class.txt"));
+			
 			if (step1 == 1)
 				{
+					System.out.println("Would you like to (1)add or (2)delete a student?");
+					int AorD = userIntInput.nextInt();
 					
-					
-					
-					
+					//if selects Add
+					if (AorD == 1)
+						{
+							
+							
+						}
+					//if selects Delete
+					else if (AorD == 2)
+						{
+							System.out.println("Who would you like to delete?");
+							String dele = userStringInput.nextLine();
+							
+							String ttt[] = dele.split(" ", 2);
+							String firstN = ttt[0];
+							String lastN = ttt[1];
+							
+							
+							while(myClass.hasNext())
+								{
+									if (Roster.roster.get(0).getFirstName().equals(firstN) && (Roster.roster.get(1).getLastName().equals(lastN)))
+										{
+											//ACTUALLY DELETE STUDENT HERE
+										}
+									else
+										{
+											System.out.println("don't recognize this student");
+										}
+								}
+						}
+					else 
+						{
+							System.out.println("command not recognised");
+						}
 				}
 			else
 				{
