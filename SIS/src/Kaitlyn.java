21c63f84aa0a2182a9c9783a317597db4eadaafe
Andrew <Andrew@DESKTOP-1MQR1M8>
2021-10-28 01:57:06 -0600

    Consolidated Kaitlyn's Code

--- SIS/src/Kaitlyn.java
+++ SIS/src/Kaitlyn.java
@@ -4,9 +4,7 @@ import java.io.IOException;
 
 public class Kaitlyn
 	{
-		
-		static Scanner string1 = new Scanner(System.in);
-		public static void mainMenu()
+		public static int mainMenu()
 		{
 			//display menu
 			
@@ -18,9 +16,26 @@ public class Kaitlyn
 			//changeStudentinfo();
 			System.out.println("	(3) sort students");
 			//sortStudents();
-			String userChoice = string1.nextLine();
+			
+			// Input checks
+			Scanner string1 = new Scanner(System.in);
+			return inputChecks(string1);
 			
 		}
 		
+		public static int inputChecks(Scanner s) {
+			boolean enteringInput = true;
+			do {
+				String userChoice = s.nextLine();
+				if (userChoice.length() != 1 || !(userChoice.charAt(0) >= 49 && userChoice.charAt(0)<=51)) {
+					System.out.println("Please enter a valid option.");
+				}
+				else {
+					enteringInput = false;
+					return Integer.parseInt(userChoice);
+				}
+			} while (enteringInput);
+			return -1;
+		}
 		
 	}
