77c9f8cfbbb48ef26618bc5d95a1e499f33d8487
Andrew <Andrew@DESKTOP-1MQR1M8.mshome.net>
2021-10-29 00:48:47 -0600

    1

--- SIS/src/RunSIS.java
+++ SIS/src/RunSIS.java
@@ -7,12 +7,17 @@ public class RunSIS {
 	public static void main(String[] args) throws IOException
 		{
 		
-			//displaymenu
-			switch (Kaitlyn.mainMenu()) {
-			case 1: // Add / delete a student (another switch structure) goes here
-			case 2: // Change grades/schedule (another switch structure) goes here
-			case 3: // Sort students (another switch structure) goes here
-			}
+		
+		Roster.fillRoster();
+		
+		//displaymenu
+		switch (Kaitlyn.mainMenu()) {
+		case 1: // Add / delete a student (another switch structure) goes here
+		case 2: // Change grades/schedule (another switch structure) goes here
+		case 3: // Sort students (another switch structure) goes here
+		}
+		
+			
 
 		
 	}
