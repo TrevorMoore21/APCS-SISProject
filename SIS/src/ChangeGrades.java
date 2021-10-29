55cf3cb22ef3ac51366431096c40e642721d44cc
andrew.markel <andrew.markel@MU-C1-01.mullen.pvt.k12.co.us>
2021-10-29 09:56:11 -0600

    switchClasses works

--- SIS/src/ChangeGrades.java
+++ SIS/src/ChangeGrades.java
@@ -48,6 +48,35 @@ public class ChangeGrades {
 	}
 	
 	public static void switchClasses() {
+		System.out.println("\nSelect a Student");
+		for (int i=0;i<Roster.roster.size();i++) {
+			System.out.println("\t(" + (i+1) + ") " + Roster.roster.get(i).getFirstName() + " " + Roster.roster.get(i).getLastName());
+		}
+		Scanner userInput = new Scanner(System.in);
+		// Do Input Checks!!
+		int studentNum = userInput.nextInt();
+		userInput.nextLine();
 		
+		System.out.println("\nSelect a Class to Change");
+		System.out.println("\t(1) " + Roster.roster.get(studentNum-1).getFirstClass()
+				+ "\n\t(2) " + Roster.roster.get(studentNum-1).getSecondClass()
+				+ "\n\t(3) " + Roster.roster.get(studentNum-1).getThirdClass());
+		switch (Kaitlyn.inputChecks(userInput, 3, 1)) {
+		case 1:
+			System.out.println("Enter the new class here:");
+			// Do Input Checks!!
+			Roster.roster.get(studentNum-1).setFirstClass(userInput.nextLine());
+			break;
+		case 2: 
+			System.out.println("Enter the new class here:");
+			// Do Input Checks!!
+			Roster.roster.get(studentNum-1).setSecondClass(userInput.nextLine());
+			break;
+		case 3: 
+			System.out.println("Enter the new class here:");
+			// Do Input Checks!!
+			Roster.roster.get(studentNum-1).setThirdClass(userInput.nextLine());
+			break;
+		}
 	}
 }
