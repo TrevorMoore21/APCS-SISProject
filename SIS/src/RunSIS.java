dee3ddd774b6c3db1a15cb1b7458bd9172a259aa
benjamin.hoody <benjamin.hoody@MU-C1-17.mullen.pvt.k12.co.us>
2021-11-01 11:37:59 -0600

    Commit this to fix problems

--- SIS/src/RunSIS.java
+++ SIS/src/RunSIS.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
 import java.io.IOException;
 
@@ -12,23 +13,59 @@ public class RunSIS {
 		Roster.fillRoster();
 		
 		//displaymenu
-		switch (Kaitlyn.mainMenu()) {
-		case 1: // Add / delete a student (another switch structure) goes here
-		case 2: 
-			switch (ChangeGrades.gradesOrSchedulePrompt()) {
-			case 1: ChangeGrades.changeGrades(); break;
-			case 2: ChangeGrades.switchClasses(); break;
+		boolean editing = true;
+		do {
+			switch (MainMenu.mainMenu()) {
+			case 1: 
+				switch (addDeleteStudent.addOrDeletePrompt()) {
+				case 1: addDeleteStudent.addStudent(); break;
+				case 2: addDeleteStudent.delStudent(); break;
+				}
+				break;
+			case 2: 
+				switch (ChangeGrades.gradesOrSchedulePrompt()) {
+				case 1: ChangeGrades.changeGrades(); break;
+				case 2: ChangeGrades.switchClasses(); break;
+				}
+				break;
+			case 3: 
+				switch (SortStudents.prompt()) {
+				case 1: Roster.roster.sort(Comparator.comparing(Student::getLastName)); break;
+//					Collections.sort(Roster.roster, new LastNameComparator()); 
+				case 2: Roster.roster.sort(Comparator.comparing(Student::getGPA).reversed()); break;
+//					Collections.sort(Roster.roster, new GpaComparator()); break;
+				case 3: 
+					switch (SortStudents.classSorterPrompt()) {
+					case 1: Roster.roster.sort(Comparator.comparing(Student::getFirstClass).thenComparing(Student::getLastName)); break;
+					case 2: Roster.roster.sort(Comparator.comparing(Student::getSecondClass).thenComparing(Student::getLastName)); break;
+					case 3: Roster.roster.sort(Comparator.comparing(Student::getThirdClass).thenComparing(Student::getLastName)); break;
+					}
+					break; 
+				}
+				System.out.println("Do you want to print the roster? (y/n)");
+				Scanner userInput = new Scanner(System.in);
+				boolean entering = true;
+				do {
+					String yesOrNo = userInput.nextLine().toLowerCase();
+					if (yesOrNo.equals("y")) {
+						Roster.printRoster();
+						entering = false;
+					}
+					else if (!yesOrNo.equals("n") && !yesOrNo.equals("y")) {
+						System.out.println("Please only enter the letters \"y\" or \"n\".");
+					}
+					else {
+						entering = false;
+					}
+				} while (entering);
+				break;
+			case 4: Roster.printRoster(); break;
+			case 5: Roster.resetRoster(); break;
+			case 6: editing = false; break;
 			}
-			break;
-		case 3: // Sort students (another switch structure) goes here
-			
-			switch (Sorting.sortingPrompt()) {
-			case 1: Sorting.sortByGPA(); break;
-			case 2: Sorting.sortByLastName(); break;
-			case 3: Sorting.sortByClass(); break;
-			}
-		}
+		} while (editing);
+		System.out.println("\nExiting...");
+		
 	}
 }
-
 
